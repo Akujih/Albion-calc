426d86f9af74b64735fc17372e82b80105911067
Akujih <henlima27@gmail.com>
2024-12-16 01:09:19 -0300

    Versão definitiva
    Totalmente funcional.
    A implementação de funcionamento do icone foi descartada por desorganizar todo o css (e estou sem pra arrumar)

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const frm = document.querySelector("form")
 const resulttable = document.querySelector("#result_table")
-//pego do form as respostas e exporta as saidas na tabela
 
 frm.addEventListener("submit" , (e)=>{
     e.preventDefault()
@@ -23,7 +22,7 @@ const c_total = ((Qtde*Valor_item)+c_totalrune)
 let c_lucro //verificador se tem premium ou não
 if(Premium == true){
     c_lucro = (((Valor_plus*Qtde)*0.925)-c_total) //com premium
-
+    
 }else{
     c_lucro = (((Valor_plus*Qtde)*0.895)-c_total) //sem premium
 
@@ -33,50 +32,25 @@ if(Premium == true){
 //cria uma nova linha 
 const new_line = document.createElement("tr")
 
-//adiciona celulas a tabela
-const qtdecell = document.createElement("td")
-qtdecell.innerText = Qtde
-//quantidade
-const namecell = document.createElement("td")
-namecell.innerText = Name_item
-//nome do item
-const valoritemcell = document.createElement("td")
-valoritemcell.innerText = Valor_item
-//valor pago
-const runeitemcell = document.createElement("td")
-runeitemcell.innerText = Rune_item
-//runa por item
-const valorRuneCell = document.createElement("td")
-valorRuneCell.innerText = Valor_rune
-//valor das runas
-const qtdeRuneCell = document.createElement("td")
-qtdeRuneCell.innerText = c_qtde
-//quantidade de runas
-const totalrunecell = document.createElement("td")
-totalrunecell.innerText = c_totalrune
-//valor total das runas
-const valorTotalCell = document.createElement("td")
-valorTotalCell.innerText = c_total
-//valor total
-const valorpluscell = document.createElement("td")
-valorpluscell.innerText = Valor_plus
-
-//valor do item aprimorado
-const lucroCell = document.createElement("td")
-lucroCell.innerText = c_lucro
-//lucro
+const cells = [
+    Qtde,
+    Name_item,
+    Valor_item,
+    Rune_item,
+    Valor_rune,
+    c_qtde,
+    c_totalrune,
+    c_total,
+    Valor_plus,
+    c_lucro
+];
 
 //adiciona novas linhas
-new_line.appendChild(qtdecell)
-new_line.appendChild(namecell)
-new_line.appendChild(valoritemcell)
-new_line.appendChild(runeitemcell)
-new_line.appendChild(valorRuneCell)
-new_line.appendChild(qtdeRuneCell)
-new_line.appendChild(totalrunecell)
-new_line.appendChild(valorTotalCell)
-new_line.appendChild(valorpluscell)
-new_line.appendChild(lucroCell)
+cells.forEach(cellValue => {
+    const cell = document.createElement("td");
+    cell.innerText = cellValue;
+    new_line.appendChild(cell);
+});
 
 // Adicionar a nova linha à tabela
 resulttable.appendChild(new_line)
